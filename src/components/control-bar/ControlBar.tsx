4cf5c795c422c3063919652c3f594e3f091115a1
Nikita <bigbasspoll@gmail.com>
2023-04-14 19:54:43 +0300

    fix xlsx export

--- src/components/control-bar/ControlBar.tsx
+++ src/components/control-bar/ControlBar.tsx
@@ -13,6 +13,28 @@ const ControlBar: FC<IControlBar> = ({ onOpen }) => {
 	const { customers } = useCustomers()
 	const [isLargerThan530] = useMediaQuery('(min-width: 530px)')
 
+	const spreadCustomersForXLSX = () => {
+		return customers.map(item => ({
+			id: item.id,
+			name: item.name,
+			email: item.email,
+			credit_limit: item.balance.credit_limit,
+			deferral_days: item.deferral_days,
+			currency: item.balance.currency,
+			current_amount: item.balance.current_amount,
+			available_amount: item.balance.available_amount,
+			org_name: item.org.name,
+			org_inn: item.org.inn,
+			org_kpp: item.org.kpp,
+			org_ogrn: item.org.ogrn,
+			org_addr: item.org.addr,
+			invoice_emails: item.invoice_emails.join(', '),
+			status: item.status,
+			created_at: item.created_at,
+			updated_at: item.updated_at
+		}))
+	}
+
 	return (
 		<Flex justifyContent={'space-between'} flexWrap={'wrap-reverse'} p={'.6em'}>
 			<Heading mt={!isLargerThan530 ? '1em' : 0} as={'h2'}>
@@ -21,7 +43,8 @@ const ControlBar: FC<IControlBar> = ({ onOpen }) => {
 			<Flex justifyContent={'space-between'} flexWrap={'wrap'}>
 				<SearchBar />
 				<Flex mt={!isLargerThan530 ? '1em' : 0} ml={!isLargerThan530 ? 0 : '1em'}>
-					<ExportCsvBtn csvData={customers} fileName={'customers-list'} />
+					<Button onClick={() => console.log(spreadCustomersForXLSX())}>Click</Button>
+					<ExportCsvBtn csvData={spreadCustomersForXLSX()} fileName={'customers-list'} />
 					<Button ml={'1em'} onClick={onOpen} leftIcon={<SmallAddIcon />}>
 						Добавить клиента
 					</Button>
